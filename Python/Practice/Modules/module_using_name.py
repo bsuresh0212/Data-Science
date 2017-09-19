7bbcd3d55c72463aedf0b92f60d4c7b4572f620e
bsuresh0212 <30489397+bsuresh0212@users.noreply.github.com>
2017-09-19 10:28:12 +0530

    Update module_using_name.py

--- Python/Practice/Modules/module_using_name.py
+++ Python/Practice/Modules/module_using_name.py
@@ -1,3 +1,12 @@
+#just run pythnon3 module_using_name.py
+#Every python module has its __name__ defined.if this is __main__ 
+#this is program stroed as a 
+    #import sys
+    #sys.path  -->stores as shows the following path as well
+#now import this file as import module_using_name
+    #run as another module
+
+
 if __name__ == '__main__':
     print("This program is being run by itself")
 else:
