9813386f49014c7f99dee81b8ad9eed28db358e1
bsuresh0212 <30489397+bsuresh0212@users.noreply.github.com>
2017-09-19 10:37:50 +0530

    Update mymodule.py

--- Python/Practice/Modules/mymodule.py
+++ Python/Practice/Modules/mymodule.py
@@ -1,4 +1,11 @@
-def say_hi():
-    print("Hi,this is my module saying HI")
+#this is program stroed as a 
+    #import sys
+    #sys.path  -->stores as shows the following path as well
+#import my_module
+# use it my_module.__version__ and
+# use it my_module.say_hi()
+# it will works good     
 
+def say_hi():
+    print('My program says Hi as imported')
 __version__ = '0.1'
