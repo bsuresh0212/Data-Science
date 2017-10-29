dd1b5d6983fe31ee36d7a4195e61008766d82c24
bsuresh0212 <sureshuce@gmail.com>
2017-10-29 16:12:14 +0530

    new changes

--- Python/Practice/HackerRank/sample.py
+++ Python/Practice/HackerRank/sample.py
@@ -23,12 +23,28 @@ print(list(arr)) '''
 from operator import itemgetter
 if __name__ == '__main__':
     students = []
+    smallest = []
+    sm1,sm2 = float('inf'),float('inf')
     for _ in range(int(input())):
         samp = []
-        name = input()
+        # name = input()
         score = float(input())
-        samp.append(name)
+        # samp.append(name)
         samp.append(score)
         students.append(samp)
     print(students)
-    print(sorted(students, key=lambda x: x[1]))
+    # sm1 = students[0][0]
+    # sm2 = students[0][0]
+    for idx,stu in enumerate(students):
+        x = stu[0]
+        if  x < sm1:
+            sm1 = x
+            if(idx == 0):
+                sm2 = sm1
+        if  (x > sm1 and sm2 >= x):
+            print(idx)
+            sm2 = x
+            print('sm2 ',sm2)
+            # smallest.append(students[idx])
+    print(smallest)
+    # students = sorted(students, key=lambda x: x[1])
